d935f85542aaf87f37558e41a4df7b26d70ed2c7
Swagat Nayak <msndipapal@gmail.com>
2020-09-19 01:52:57 +0530

    Updated the code

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ client = discord.Client()
 
 bot_token = config('TOKEN')
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 prefix = ';'
 
 check = False
+
 
 @client.event
 async def on_message(message):
@@ -19,26 +22,31 @@ async def on_message(message):
         global check
         check = True
 
-@client.event
-async def on_message(message):
 
-    send = ""
+try:
+    @client.event
+    async def on_message(message):
 
-    todo = message.content.split(prefix , 1)[1].split(' ' + prefix )
+        send = ""
 
-    if message.author == client.user:
-        return
+        todo = (message.content.split(prefix, 1)[1] + ' ;end').split(' ' + prefix)
 
-    if message.content.startswith(prefix + 'hello' or prefix + 'hi'):
-        await message.channel.send('Hello! ' + message.author.name)
+        if message.author == client.user:
+            return
 
-    if message.content.startswith( prefix + 'spam'):
-        for i in range(int(todo[2])):
-            global check
-            await message.channel.send(todo[1])
-            if check:
-                check = False
-                break
+        if message.content.startswith(prefix + 'hello' or prefix + 'hi'):
+            await message.channel.send('Hello! ' + message.author.name)
 
-
+        if message.content.startswith(prefix + 'spam'):
+            try:
+                for i in range(int(todo[2])):
+                    global check
+                    await message.channel.send(todo[1])
+                    if check:
+                        check = False
+                        break
+            finally:
+                print(IndexError)
+finally:
+    print(Exception)
 client.run(bot_token)
